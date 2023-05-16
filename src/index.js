54bae8bade3d0dc34631d99e0adedc389d2d7bc7
Harbon486 <wxwnwo8h@ethan796.4wrd.cc>
2023-05-16 00:34:40 -0700

    Snapshots with React.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+We are seeking a skilled and experienced React developer to assist us with a short-term project to integrate React into our existing Vue3 application. The project length is expected to be less than one month. The ideal candidate should be proficient in JavaScript, React, React Native, Vue3 and Tailwind CSS.
+
+Be more exact, you need to port one React page into existing vue3 page with tailwind css. The react page talk with next.js originally. And this part, you need to make the vue3 page to talk with next.js instead.
+
+One more thing, we need to add a user sign in for new vue3 page.
+
+We are looking for a skilled CSS developer who can help us fix some design issues in our web application. The ideal candidate should have experience in CSS, HTML, and JavaScript and should be able to work on this project for less than a month.""
